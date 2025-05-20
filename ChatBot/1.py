22b8b2a44a3f337c895f8d96d734239fe33208da
Mudit Goswami <154737546+Goswami-Mudit@users.noreply.github.com>
2025-05-20 09:46:57 +0530

    Update 1.py

--- ChatBot/1.py
+++ ChatBot/1.py
@@ -1,3 +1,4 @@
+
 from langchain_openai import ChatOpenAI  
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
@@ -8,7 +9,7 @@ st.title("YOUR PERSONAL CHATBOT🤖")
 llm = ChatOpenAI(
     base_url="https://openrouter.ai/api/v1",
     model_name="deepseek/deepseek-r1:free",
-    api_key=st.secrets["openrouter_api_key"],
+    api_key="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",
     temperature=0.7
 )
 
@@ -33,3 +34,4 @@ if prompt := st.chat_input("Ask anything!"):
     
     st.session_state.messages.append({"role": "assistant", "content": response})
     st.chat_message("assistant").write(response)
+ 
